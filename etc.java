086b7a6593cab4339410fae9037b27cc32a12c46
timothyhartanto <hartantotimothy@gmail.com>
2015-11-16 23:33:02 +0700

    Create etc.java

---
+++ etc.java
@@ -0,0 +1,14 @@
+package com.example.proto.listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class etc extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.etc);
+    }
+}
