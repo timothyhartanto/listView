1cf3714e73a5b2d7282324436fa95666a0e6054e
timothyhartanto <hartantotimothy@gmail.com>
2015-11-16 23:33:12 +0700

    Create menu.java

---
+++ menu.java
@@ -0,0 +1,13 @@
+package com.example.proto.listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class menu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
